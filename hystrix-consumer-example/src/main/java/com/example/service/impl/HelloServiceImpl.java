03c6d8e0f874e7e21bbc02b6a2110917214fb465
ynFatal <63413763@qq.com>
2020-07-14 05:43:40 +0800

    add configuration for @HystrixCommand.

--- hystrix-consumer-example/src/main/java/com/example/service/impl/HelloServiceImpl.java
+++ hystrix-consumer-example/src/main/java/com/example/service/impl/HelloServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.service.impl;
 
 import com.example.service.IHelloService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -30,5 +31,26 @@ public class HelloServiceImpl implements IHelloService {
         return "helloFallback";
     }
 
+    /**
+     * Spring Cloud 会将带有 @HystrixCommand 注解的 Spring bean 包装在一个连接到 Hystrix 断路器的代理中.
+     * 要配置 @HystrixCommand，可以将 commandProperties 属性或 threadPoolProperties 属性与 @HystrixProperty 注解列表一起使用。请参阅下边链接了解更多详情：
+     * https://github.com/Netflix/Hystrix/tree/master/hystrix-contrib/hystrix-javanica#configuration
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "helloFallback",
+        commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
+        },
+        threadPoolProperties = {
+            @HystrixProperty(name = "coreSize", value = "30"),
+            @HystrixProperty(name = "maxQueueSize", value = "101"),
+            @HystrixProperty(name = "keepAliveTimeMinutes", value = "2"),
+            @HystrixProperty(name = "queueSizeRejectionThreshold", value = "15"),
+            @HystrixProperty(name = "metrics.rollingStats.numBuckets", value = "12"),
+            @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "1440")
+        })
+    public String hello2() {
+        return restTemplate.getForObject("http://HYSTRIX-PROVIDER-EXAMPLE/hello", String.class);
+    }
 
 }
