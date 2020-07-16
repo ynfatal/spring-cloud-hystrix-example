0eec05e86b40be8621eb766d9de5f3976a040c6a
ynFatal <634137063@qq.com>
2020-07-16 08:37:17 +0800

    add notes.(to be tested)

--- hystrix-dashboard-example/src/main/java/com/example/HystrixDashboardExampleApplication.java
+++ hystrix-dashboard-example/src/main/java/com/example/HystrixDashboardExampleApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableHystrix
 @EnableHystrixDashboard
 @EnableEurekaClient
 public class HystrixDashboardExampleApplication {
@@ -24,6 +22,7 @@ public class HystrixDashboardExampleApplication {
      * https://blog.csdn.net/ddxd0406/article/details/79643059?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase
      * https://blog.csdn.net/fxbin123/article/details/82322476?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase
      * https://blog.csdn.net/dfshsdr/article/details/97762338?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase
+     * https://www.freesion.com/article/8641843040/
      * @return
      */
     @Bean
@@ -31,7 +30,7 @@ public class HystrixDashboardExampleApplication {
         HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
         registrationBean.setLoadOnStartup(1);
-        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
         registrationBean.setName("HystrixMetricsStreamServlet");
         return registrationBean;
     }
