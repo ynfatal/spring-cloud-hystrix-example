95966f002ec15a096f97cf890f9d157b2c09727c
ynFatal <634137063@qq.com>
2020-07-17 08:27:14 +0800

    remove useless annotation, import and dependency.

--- hystrix-dashboard-example/src/main/java/com/example/HystrixDashboardExampleApplication.java
+++ hystrix-dashboard-example/src/main/java/com/example/HystrixDashboardExampleApplication.java
@@ -4,14 +4,12 @@ import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServl
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableHystrixDashboard
-@EnableCircuitBreaker
 @EnableEurekaClient
 public class HystrixDashboardExampleApplication {
 
@@ -23,8 +21,6 @@ public class HystrixDashboardExampleApplication {
      * SpringBoot2.0 下 hystrix dashboard Unable to connect to Command Metric Stream 解决办法，参考文章：
      * https://blog.csdn.net/ddxd0406/article/details/79643059?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase
      * https://blog.csdn.net/fxbin123/article/details/82322476?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase
-     * https://blog.csdn.net/dfshsdr/article/details/97762338?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase
-     * https://www.freesion.com/article/8641843040/
      * @return
      */
     @Bean
