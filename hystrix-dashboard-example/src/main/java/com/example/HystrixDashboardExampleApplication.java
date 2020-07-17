9681a82a506e7497bc66e00a905abcca0be76fa9
ynFatal <634137063@qq.com>
2020-07-17 08:52:50 +0800

    remove useless bean, dependency and configuration.

--- hystrix-dashboard-example/src/main/java/com/example/HystrixDashboardExampleApplication.java
+++ hystrix-dashboard-example/src/main/java/com/example/HystrixDashboardExampleApplication.java
@@ -1,13 +1,14 @@
 package com.example;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.context.annotation.Bean;
 
+/**
+ * hystrix dashboard Unable to connect to Command Metric Stream 解决办法，参考文章：
+ * https://blog.csdn.net/fxbin123/article/details/82322476?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase
+ */
 @SpringBootApplication
 @EnableHystrixDashboard
 @EnableEurekaClient
@@ -15,22 +16,6 @@ public class HystrixDashboardExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HystrixDashboardExampleApplication.class, args);
-    }
-
-    /**
-     * SpringBoot2.0 下 hystrix dashboard Unable to connect to Command Metric Stream 解决办法，参考文章：
-     * https://blog.csdn.net/ddxd0406/article/details/79643059?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase
-     * https://blog.csdn.net/fxbin123/article/details/82322476?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase
-     * @return
-     */
-    @Bean
-    public ServletRegistrationBean getServlet() {
-        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
-        registrationBean.setLoadOnStartup(1);
-        registrationBean.addUrlMappings("/actuator/hystrix.stream");
-        registrationBean.setName("HystrixMetricsStreamServlet");
-        return registrationBean;
     }
 
 }
