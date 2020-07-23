844d0b50645d9f2a87ae2bf0320d4102716e2813
ynFatal <63413763@qq.com>
2020-07-23 22:35:51 +0800

    add notes and modify name.

--- circuitbreak-consumer-example/src/main/java/com/example/config/CircuitBreakerGenericConfiguration.java
+++ circuitbreak-consumer-example/src/main/java/com/example/config/CircuitBreakerGenericConfiguration.java
@@ -11,13 +11,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 /**
+ * CircuitBreaker 通用配置
+ * 局限于使用 org.springframework.cloud.netflix.hystrix.HystrixCircuitBreakerFactory#create(java.lang.String) 方法创建的断路器
  * @author Fatal
  * @date 2020/7/23 0023 22:00
  */
 @Configuration(proxyBeanMethods = false)
-@Profile(value = "circuitbreaker_global_configuration")
-public class CircuitBreakerGlobalConfiguration {
+@Profile(value = "circuitbreaker_generic_configuration")
+public class CircuitBreakerGenericConfiguration {
 
+    /**
+     * @apiNote 为所有断路器提供默认配置，该 bean 将传递给 HystrixCircuitBreakerFactory。具体可以参考自动配置类源码：
+     * org.springframework.cloud.netflix.hystrix.HystrixCircuitBreakerAutoConfiguration，可以理解为行为嵌套封装。
+     * configureDefault 方法可以用于提供默认配置，它真正起到的作用是将行为 defaultConfiguration 封装到 HystrixCircuitBreakerFactory 中。
+     * @return
+     */
     @Bean
     public Customizer<HystrixCircuitBreakerFactory> customizer() {
         return factory -> factory.configureDefault(id -> HystrixCommand.Setter
